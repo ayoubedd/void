367f321ad97c93fb49ab7aeb84f42271768f418d
Ayoub Eddaoudi <me@ayoubedd.me>
2025-08-26 12:03:49 +0100

    feat: update quick sort implementation

--- rust/src/sorting/quick.rs
+++ rust/src/sorting/quick.rs
@@ -1,29 +1,48 @@
-fn quick_sort<T: PartialOrd + Clone>(input: &mut [T]) {
-    if input.len() <= 1 {
+fn partition<T: PartialOrd>(array: &mut [T]) -> usize {
+    let mut lo: usize = 0;
+    let mut hi: usize = array.len() - 2;
+    let pivot_idx: usize = array.len() - 1;
+
+    if array.len() <= 1 {
+        return 0;
+    }
+
+    loop {
+        while lo <= hi && array[lo] <= array[pivot_idx] {
+            lo += 1;
+        }
+
+        while hi >= lo && array[hi] > array[pivot_idx] {
+            if hi == 0 {
+                break;
+            }
+            hi -= 1;
+        }
+
+        if lo >= hi || hi <= lo {
+            break;
+        }
+
+        array.swap(lo, hi);
+
+        lo += 1;
+        hi -= 1;
+    }
+
+    array.swap(lo, pivot_idx);
+
+    lo
+}
+
+fn quick_sort<T: PartialOrd>(array: &mut [T]) {
+    if array.len() <= 1 {
         return;
     }
 
-    let pivot = input[input.len() - 1].clone();
+    let pivot_new_index = partition(array);
 
-    let mut i: usize = 0;
-    let mut j: usize = 0;
-
-    loop {
-        if input[j] <= pivot {
-            input.swap(j, i);
-            i += 1;
-        }
-
-        j += 1;
-        if j == input.len() - 1 {
-            break;
-        }
-    }
-
-    input.swap(j, i);
-
-    quick_sort(&mut input[..i]);
-    quick_sort(&mut input[i + 1..]);
+    quick_sort(&mut array[..pivot_new_index]);
+    quick_sort(&mut array[pivot_new_index + 1..]);
 }
 
 #[cfg(test)]
@@ -32,8 +51,8 @@ mod tests {
 
     #[test]
     fn basic() {
-        let mut nums = [-100, 2, 1, -10];
+        let mut nums = [200, -100, 2, 1, -10];
         quick_sort(&mut nums);
-        assert_eq!(nums, [-100, -10, 1, 2]);
+        assert_eq!(nums, [-100, -10, 1, 2, 200]);
     }
 }
